8b9f8cb7b0f1946f29e2578e6e3eaa64da2b7fba
Ahmed Gamaleldin <ahmedgamal1496@gmail.com>
2025-01-07 15:54:15 +0200

    use config service to acces env variables

--- src/rabbitmq/rabbitmq.module.ts
+++ src/rabbitmq/rabbitmq.module.ts
@@ -4,15 +4,19 @@ import { RabbitmqService } from './rabbitmq.service';
 
 @Module({
   imports: [
-    RabbitMQModule.forRoot(RabbitMQModule, {
-      exchanges: [
-        {
-          name: 'ticktalk_exchange',
-          type: 'direct',
-        },
-      ],
-      uri: process.env.RABBITMQ_URI,
-      connectionInitOptions: { wait: false },
+    ConfigModule,
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        exchanges: [
+          {
+            name: 'ticktalk_exchange',
+            type: 'direct',
+          },
+        ],
+        uri: configService.get<string>('RABBITMQ_URI'), // Access environment variable
+        connectionInitOptions: { wait: false },
+      }),
     }),
   ],
   providers: [RabbitmqService],
